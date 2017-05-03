d4f904a96eb99b4dfcb76267e1b9a19285447d61
Unknown <egr274@nyu.edu>
2017-05-03 16:00:36 -0400

    stuff

---
+++ Assets/ChangeScenes.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScenes : MonoBehaviour {
+    public string sceneName;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnMouseUpAsButton()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
