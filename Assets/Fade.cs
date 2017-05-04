ee02ffc11af737200c951b94af3768c0e6f2acba
Unknown <aaronshenglu@gmail.com>
2017-05-04 01:38:02 -0400

    Created fade-out gameOver screen.

---
+++ Assets/Fade.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Fade : MonoBehaviour {
+
+	public float fadeSpeed = 2f;
+
+	public bool fadingOut = false;
+	SpriteRenderer ourSprite;
+
+	public string sceneToSwitchToWhenDoneFadingIn = "ActualGame";
+
+	void Start() {
+		ourSprite = GetComponent<SpriteRenderer>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (fadingOut) {
+			float alpha = ourSprite.color.a;
+			alpha += fadeSpeed*Time.deltaTime;
+			if (alpha > 1f) {
+				alpha = 1;
+				SceneManager.LoadScene(sceneToSwitchToWhenDoneFadingIn);
+			}
+			ourSprite.color = new Color(ourSprite.color.r, ourSprite.color.g, ourSprite.color.b, alpha);
+		}
+		else {
+			float alpha = ourSprite.color.a;
+			alpha -= fadeSpeed*Time.deltaTime;
+			if (alpha < 0f) {
+				alpha = 0;
+			}
+			ourSprite.color = new Color(ourSprite.color.r, ourSprite.color.g, ourSprite.color.b, alpha);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			fadingOut = !fadingOut;
+		}
+
+	}
+}
