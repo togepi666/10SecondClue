f6ec09a65648231ea23f6b52b0b30c859e350b2b
Unknown <allisongameyfuntimes@gmail.com>
2017-05-08 14:42:14 -0400

    almost looks like a game

---
+++ Assets/timer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class timer : MonoBehaviour {
+    int number = 10;
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("countDownFunction", 1, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<TextMesh>().text = "" + number;
+        if(number <= 0)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+	}
+
+    void  countDownFunction()
+    {
+        number--;
+    }
+    
+}
